7e7aa63e9e0052baa0c6a9ecdd6f149f8f7f8c7e
Jackson <jackson1712@hotmail.com>
2023-04-18 16:37:42 -0300

    to fix

--- src/server.js
+++ src/server.js
@@ -1,6 +1,29 @@
 import http from "node:http";
+import { json } from "./middlewares/json.js";
+import { routes } from "./middlewares/routes.js";
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
+  const { method, url } = req;
+
+  await json(req, res);
+
+  const route = routes.find(route => {
+    return route.method === method && route.path === url
+  });
+
+  try {
+    if (!route) {
+      throw new Error("Rota http não encontrada")
+    }
+    if (route) {
+      return route.handler(req, res);
+    }
+
+  } catch (error) {
+    console.error(error);
+
+    return res.writeHead(404).end("Rota não encontrada");
+  }
 
 });
 

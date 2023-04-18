3ec8c4d67e447e267572f1b6d62ec5cbd5ab703e
Jackson <jackson1712@hotmail.com>
2023-04-18 16:36:34 -0300

    created file to control requisitions http

---
+++ src/middlewares/routes.js
@@ -0,0 +1,43 @@
+import { Database } from "../database.js";
+import { randomUUID } from "node:crypto";
+
+const database = new Database();
+
+export const routes = [
+  {
+    method: 'GET',
+    path: '/tasks',
+    handler: (req, res) => {
+      const tasks = database.select("tasks")
+
+      return res.end(JSON.stringify(tasks));
+    }
+  },
+  {
+    method: 'POST',
+    path: '/tasks',
+    handler: (req, res) => {
+      const { title, description } = req.body;
+
+      const task = {
+        id: randomUUID(),
+        title,
+        description,
+        completed_at: null,
+        created_at: new Date(),
+        updated_at: null
+      }
+  
+      database.insert("tasks", task);
+  
+      return res.writeHead(201).end();
+    }
+  },
+  {
+    method: 'DELETE',
+    path: '/tasks',
+    handler: (req, res) => {
+
+    }
+  }
+]
